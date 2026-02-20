c97dc12dfcc45be90851be9fa68f8284a9addb23
Pedro <pedroguedescs@gmail.com>
2026-02-20 18:44:35 -0300

    Add projects preview section + content setup

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from "astro:content";
+
+const projects = defineCollection({
+  type: "data",
+  schema: ({image}) => z.object({
+      category: z.enum(["web", "cli", "other"]),
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).default([]),
+      repo: z.string().url().optional(),
+      website: z.string().url().optional(),
+      demo: z.string().url().optional(),
+      featured: z.boolean().default(false),
+      image: image()
+    }),
+});
+
+const skills = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    category: z.enum(["frontend", "backend", "tools", "other"]),
+  }),
+});
+
+export const collections = { projects, skills };
